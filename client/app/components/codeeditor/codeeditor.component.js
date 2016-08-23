86824704b6c9e5a3a27e971a5877476f8853b58b
Leigh Boone 580453 <boone_leigh@bah.com>
2016-08-23 18:17:54 -0400

    updating toggle

--- client/app/components/codeeditor/codeeditor.component.js
+++ client/app/components/codeeditor/codeeditor.component.js
@@ -1,18 +1,14 @@
 'use strict';
 const angular = require('angular');
 
-export class codeeditorComponent {
-  /*@ngInject*/
-  constructor() {
-  	this.isCollapsed = true;
-  }
-}
-
 export default angular.module('aliasTreehouseApp.codeeditor', [])
-  .component('codeeditor', {
-    template: require('./codeeditor.html'),
-    // bindings: { message: '<' },
-    controller: codeeditorComponent,
-    selector: 'codeeditor'
-  })
-  .name;
+    .component('codeeditor', {
+        template: require('./codeeditor.html'),
+        controller: function() {
+            this.isCollapsed = true;
+        },
+        bindings: {
+	        isCollapsed: '<'
+	    }
+    })
+    .name;
