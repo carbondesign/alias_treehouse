2bbe3201459144c22deaba68ace65d3a907cbe47
Leigh Boone 580453 <boone_leigh@bah.com>
2016-08-27 10:18:28 -0400

    editor working

--- client/app/components/codeeditor/codeeditor.component.js
+++ client/app/components/codeeditor/codeeditor.component.js
@@ -6,6 +6,20 @@ export default angular.module('aliaApp.codeeditor', [])
         template: require('./codeeditor.html'),
         controller: function() {
             this.isCollapsed = true;
+	        var editor = ace.edit("editor");
+	        editor.setTheme("ace/theme/twilight");
+	        editor.session.setMode("ace/mode/html");
+
+	        // function showHTML() {
+	        //     document.getElementById('return').html(editor.getValue());
+	        // }
+	        // or use data: url to handle things like doctype
+	        function showHTMLInIFrame() {
+	            document.getElementById('results').innerHTML = ("<iframe src=" +
+	                 "data:text/html," + encodeURIComponent(editor.getValue()) +
+	            "></iframe>");
+	        }
+	        editor.on("input", showHTMLInIFrame)
         },
         bindings: {
 	        isCollapsed: '<'
