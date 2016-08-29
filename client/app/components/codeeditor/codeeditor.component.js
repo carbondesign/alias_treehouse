a7941e3646a52688d9563bdef1670a10366d8a69
Leigh Boone 580453 <boone_leigh@bah.com>
2016-08-29 10:13:31 -0400

    latest

--- client/app/components/codeeditor/codeeditor.component.js
+++ client/app/components/codeeditor/codeeditor.component.js
@@ -1,5 +1,6 @@
 'use strict';
 const angular = require('angular');
+import filesaver from 'file-saver'
 
 export default angular.module('aliaApp.codeeditor', [])
     .component('codeeditor', {
@@ -9,15 +10,19 @@ export default angular.module('aliaApp.codeeditor', [])
 	        var editor = ace.edit("editor");
 	        editor.setTheme("ace/theme/twilight");
 	        editor.session.setMode("ace/mode/html");
+	        this.saveFile = function(){
+	        	var editorContent = editor.getValue();
+	        	var blob = new Blob([editorContent], {type: "text/plain;charset=utf-8"});
+				filesaver.saveAs(blob, "poster.html");
 
-	        // function showHTML() {
-	        //     document.getElementById('return').html(editor.getValue());
-	        // }
+				// var file = new File([editorContent], "poster.html", {type: "text/plain;charset=utf-8"});
+				// filesaver.saveAs(file);
+	        }
 	        // or use data: url to handle things like doctype
 	        function showHTMLInIFrame() {
 	            document.getElementById('results').innerHTML = ("<iframe src=" +
-	                 "data:text/html," + encodeURIComponent(editor.getValue()) +
-	            "></iframe>");
+	                "data:text/html," + encodeURIComponent(editor.getValue()) +
+	            	" width='275px' height='350px' id='content'></iframe>");
 	        }
 	        editor.on("input", showHTMLInIFrame)
         },
